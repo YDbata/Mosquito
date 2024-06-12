169cea294edf09667eaf93d5581022d8a98af523
Y_ik sun <51112432+YDbata@users.noreply.github.com>
2024-06-12 12:12:53 +0900

    feat : 모기 움직임 수정
    모기 rotate 수정

--- Assets/2.Script/Mosquito/MouseMove.cs
+++ Assets/2.Script/Mosquito/MouseMove.cs
@@ -9,6 +9,7 @@ public class MouseMove : MonoBehaviour
     //private Vector3 mosquitoPosition;
     public GameObject mosquito;
     [SerializeField] private float speed = 5f;
+    [SerializeField]private float rotationspeed = 1f;
     [Header("바라보는 각도 조절")] public float zPositionAlpha;
 
     private Rigidbody rb;
@@ -25,11 +26,18 @@ public class MouseMove : MonoBehaviour
         Vector3 mosquitoPosition = mosquito.transform.position;
              
         // 마우스의 z축
-        mPosition.z = mosquitoPosition.z - (mosquitoPosition.z - Camera.main.transform.position.z); //Mathf.Tan(Mathf.Asin((mPosition.x - mosquitoPosition.x)/0.15f))*(mPosition.x - mosquitoPosition.x);//- Camera.main.transform.position.z;
+        //mPosition.z = mosquitoPosition.z - (mosquitoPosition.z - Camera.main.transform.position.z); //Mathf.Tan(Mathf.Asin((mPosition.x - mosquitoPosition.x)/0.15f))*(mPosition.x - mosquitoPosition.x);//- Camera.main.transform.position.z;
 
         
         // 화면의 픽셀단위 위치를 유니티 좌표로 변환
-        Vector3 target = Camera.main.ScreenToWorldPoint(mPosition);
+        // ScreenToViewportPoint : Nomal로 들어옴
+        // mosquito 가 0.5,0.5 로 두고 모기 기준 target을 비교하여 좌우 상하를 구해서 
+        // target.x*회전속도* mosquito.transform.farword
+        Vector2 viewportPoint = Camera.main.ScreenToViewportPoint(mPosition);
+        Vector3 target = mosquito.transform.position + 
+                         mosquito.transform.forward + (mosquito.transform.right * (viewportPoint.x-0.5f)*rotationspeed) 
+                                                    + (Vector3.up * (viewportPoint.y-0.5f)*rotationspeed);
+        //Vector3 target = Camera.main.ScreenToViewportPoint(mPosition);
              
         float dy = target.y - mosquitoPosition.y;
         float dx = target.x - mosquitoPosition.x;
@@ -40,7 +48,9 @@ public class MouseMove : MonoBehaviour
 
 
         Debug.Log(target+" "+mosquito.transform.position);
-        transform.LookAt(target);
+        transform.rotation *= Quaternion.Euler(Vector3.left * (viewportPoint.y - 0.5f) * rotationspeed);
+        transform.rotation *= Quaternion.Euler(Vector3.up*(viewportPoint.x-0.5f)*rotationspeed);
+        //transform.LookAt(target);
         
         // 위아리좌우 이동 구현
         //Debug.Log(dx+" "+dy);
